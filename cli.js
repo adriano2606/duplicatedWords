c4b73ad989f95090a4f6d36501f861c2d5ee4ae6
Adriano de Camargo <adrianodecamargo123@gmail.com>
2024-08-05 08:10:07 -0300

    Create file when reading

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { getDuplicatedWordsList } from './index/processTextData.js'; 
 import { processError } from './errors/erroProcessing.js';
-import {outputDuplicatedWords} from './index/processResult.js'
+import {outputDuplicatedWords, createFile} from './index/processResult.js'
 
 const archiveToRead = process.argv[2]
 
@@ -9,7 +9,8 @@ fs.readFile(archiveToRead,'UTF-8', (error,data)=>{
     try{
         if (error) throw error
         const duplicatedWordsPerParagraph = getDuplicatedWordsList(data)
-        outputDuplicatedWords(duplicatedWordsPerParagraph)
+        const output = outputDuplicatedWords(duplicatedWordsPerParagraph)
+        createFile(output)
     } catch (error){
         processError(error)
     }
--- index/processResult.js
+++ index/processResult.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 function filterForDuplicatedWords(paragraphCount){
     return Object.keys(paragraphCount).filter(word => paragraphCount[word] > 1)
 }
@@ -13,8 +15,20 @@ function outputDuplicatedWords(paragraphList){
             output += `Repeated Words in paragraph ${index + 1}: ${duplicatedWordsWithCount.join(', ')}\n`
         }
     })
-    console.log(output);
     return output
 }
 
-export {outputDuplicatedWords}
+async function createFile(content){
+    const archiveRead = process.argv[2].replace('./archives/','')
+    const pathToSave = './results'
+    const totalArchivesOnDir = fs.readdirSync(pathToSave).length
+    const newFilePath = `${pathToSave}/result${totalArchivesOnDir+1}-${archiveRead}`
+    try{
+        await fs.promises.writeFile(newFilePath,content)
+        console.log(`File sucessfully created!\nFile path:${newFilePath}`);
+    } catch (error){
+        console.log(error);
+    }
+}
+
+export {outputDuplicatedWords, createFile}
